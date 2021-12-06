9b07099e91913a2a99d19c4bb0522a94337d1362
R. Nick Vandemark <rnvandemark@gmail.com>
2021-12-06 15:13:45 -0500

    Refs #2.20: add placeholder ROS test node and launch files

---
+++ test/TestMain.cpp
@@ -0,0 +1,40 @@
+/**
+ * @file TestMain.cpp
+ * @author Robert Vandemark
+ * @brief The testing suite's entry point.
+ * @version 0.1
+ * @date 2021-12-06
+ *
+ * @copyright Copyright 2021 Robert Vandemark, Aditya Jadhav, Abhishek Nalawade
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <ros/ros.h>
+#include <gtest/gtest.h>
+
+#include <memory>
+
+std::unique_ptr<ros::NodeHandle> nh;
+
+TEST(TESTSuite, testMain) {
+    const bool dummy = true;
+    EXPECT_TRUE(dummy);
+}
+
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "test_main");
+    nh.reset(new ros::NodeHandle);
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
