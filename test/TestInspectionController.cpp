ad811ff6cabb67bb1778428afaa4cecd5d83506e
R. Nick Vandemark <rnvandemark@gmail.com>
2021-12-13 07:33:54 -0500

    Refs #3.15: continue adding inspection controller unit tests

--- test/TestInspectionController.cpp
+++ test/TestInspectionController.cpp
@@ -33,6 +33,7 @@
 #include "enpm808x_final_inspection_robot/InspectionMetrics.h"
 
 // Help shorten typename
+using Enpm808xIm = enpm808x_final_inspection_robot::InspectionMetrics;
 template <typename T> using ActionSrv = actionlib::SimpleActionServer<T>;
 template <typename T> using ActionCli = actionlib::SimpleActionClient<T>;
 
@@ -44,6 +45,14 @@ void armCallbackBad(
 void armCallbackGood(
         const control_msgs::FollowJointTrajectoryGoalConstPtr& goal,
         ActionSrv<control_msgs::FollowJointTrajectoryAction>* sas) {
+    sas->setSucceeded();
+}
+
+void baseCallbackGood(
+        const move_base_msgs::MoveBaseGoalConstPtr& goal,
+        ActionSrv<move_base_msgs::MoveBaseAction>* sas) {
+    static const ros::Duration sleep_duration(0.5);
+    sleep_duration.sleep();
     sas->setSucceeded();
 }
 
@@ -90,6 +99,92 @@ TEST(TESTSuite, testArmTrajectoryResult) {
     }
 
 #undef DO_ARM_TEST
+}
+
+TEST(TESTSuite, testInspect) {
+    // The values of these don't matter
+    const geometry_msgs::Pose hp;
+    const tf::Transform dpo;
+    // Set constants
+    const double timeout = 5.0;
+    const ros::Duration sleep_duration(0.1);
+
+    ActionSrv<move_base_msgs::MoveBaseAction> move_base_action_srv(
+        *nh,
+        "/move_base",
+        boost::bind(&baseCallbackGood, _1, &move_base_action_srv),
+        true);
+    ActionCli<move_base_msgs::MoveBaseAction> move_base_action_cli(
+        *nh,
+        "/move_base",
+        false);
+
+#define DO_INSPECT_TEST(positions) \
+    InspectionController cont(*nh, hp, dpo); \
+    bool finished = false; \
+    std::vector<Enpm808xIm> results; \
+    ros::Subscriber inspection_metrics_sub = nh->subscribe<Enpm808xIm>( \
+        "inspection_metrics", \
+        1000, \
+        [&](const Enpm808xIm::ConstPtr& msg) { \
+            results.push_back(*msg); \
+        }); \
+    ros::Subscriber finished_sub = nh->subscribe<std_msgs::Empty>( \
+        "inspection_finished", \
+        1, \
+        [&](const std_msgs::EmptyConstPtr& msg) { \
+            (void)msg; \
+            finished = true; \
+        }); \
+    const std::vector<tf::Vector3> can_positions = positions; \
+    cont.inspect(can_positions); \
+    const ros::Time start = ros::Time::now(); \
+    while (!finished && (timeout > (ros::Time::now() - start).toSec())) { \
+        sleep_duration.sleep(); \
+        ros::spinOnce(); \
+    } \
+    EXPECT_TRUE(finished); \
+    ASSERT_EQ(results.size(), (can_positions.size() + 1)); \
+    int idx = 0; \
+    for (auto iter = results.begin(); iter != results.end(); ++iter) { \
+        const geometry_msgs::Pose result = iter->transform_WDi_expected; \
+        const tf::Vector3 position = can_positions[idx++]; \
+        EXPECT_NEAR(result.position.x, position.getX(), 0.000001); \
+        EXPECT_NEAR(result.position.y, position.getY(), 0.000001); \
+        EXPECT_NEAR(result.position.z, 0.0, 0.000001); \
+    } \
+    inspection_metrics_sub.shutdown(); \
+    finished_sub.shutdown()
+
+    // Create scoped tests to help destroy the objects with these random values
+    // (the exact values shouldn't matter, just that the inputs and outputs
+    // agree)
+    {
+        DO_INSPECT_TEST(std::vector<tf::Vector3>({
+            tf::Vector3(1.0, 2.0, 3.0)
+        }));
+    }
+    {
+        DO_INSPECT_TEST(std::vector<tf::Vector3>({
+            tf::Vector3(1.0, 2.0, 3.0),
+            tf::Vector3(-3.0, -2.0, -1.0),
+            tf::Vector3(0.0, 0.0, 0.0),
+            tf::Vector3(1.2, 3.4, 5.6)
+        }));
+    }
+    {
+        DO_INSPECT_TEST(std::vector<tf::Vector3>({
+            tf::Vector3(42.42, 24.24, -42.24),
+            tf::Vector3(-6.7, 0.0, 0.0),
+            tf::Vector3(1.1, 2.3, 5.8),
+            tf::Vector3(-1.1, 3.3, 5.5),
+            tf::Vector3(100.1, -99.9, 6.66),
+            tf::Vector3(33.3, 6.67, 100.0),
+            tf::Vector3(-3.14, 6.022, 6.634)
+        }));
+    }
+
+#undef DO_INSPECT_TEST
 }
 
 int main(int argc, char** argv) {
